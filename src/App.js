06699febe3f8ff27c57315b6d6b7962fe1808c7c
luu-jennifer <jenniferluu87@gmail.com>
2022-10-18 08:45:28 -0700

    added checkbox and light mode function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // any other logic can go INSIDE  the component but OUTSIDE of the return
+  function handleClick() {
+    console.log('Checkbox has been checked ✅');
+  }
+
+  // can also be written like this:
+  // const handleClick = () => {
+  //   console.log('Checkbox has been checked ✅');
+  // }
+
+  // this is what will be redered on the page
   return (
     <div className="App">
       <form action="">
@@ -9,7 +20,7 @@ function App() {
           <legend>Choose your theme:</legend>
           
           <label htmlFor="theme"></label>
-          <input type="checkbox" name="theme" id="theme" />
+          <input type="checkbox" name="light" value="light" onClick={ handleClick }  id="theme" />
         </fieldset>
       </form>
     </div>
